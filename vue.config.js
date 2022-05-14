ac68af91ce2a6fa501c7aa65960ca933eb5149db
Shin <bear55121677@gmail.com>
2022-05-14 08:17:17 +0800

    update public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["quasar"],
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/divination/" : "/",
   pluginOptions: {
     quasar: {
       importStrategy: "kebab",
